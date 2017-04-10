4c7c9fd37b89f2c996ed747d30c6ec9bf4b53ffe
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-10 15:27:56 -0500

    wrote login router

---
+++ routes/loginRt.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const {Router} = require('express');
+const {show} = require('./controllers/loginCtrl');
+
+const loginRouter = Router();
+
+loginRouter.get('/login', show);
+
+module.exports = loginRouter;
