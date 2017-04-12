2fc41d8cd79984bb64f39be012990425873461b3
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-12 11:19:27 -0500

    configed passport strategies to resemeble pizza shacks

--- passport-strategies.js
+++ passport-strategies.js
@@ -1,1 +1,41 @@
 'use strict'
+
+const passport = require('passport');
+const {Strategy} = require('passport-local');
+const {knex} = require('./db/database.js');
+
+const User = require('./models/userMd.js');
+
+passport.serializeUser((user, done)=> done(null, user.id));
+
+passport.deserializeUser((id, done))=>{
+  knex('users').where({id}).first()
+  .then((user)=>{done(null, user)})
+  .catch((err)=>{done(err, null)})
+}
+
+const localStrategy = new Strategy({
+  usernameField: 'email',
+  passwordField: 'password'
+  },
+  (email, passwordStr, done) => {
+    User.findOneByEmail(email)
+    .then((user)=>{
+      if (user) {
+        return Promise.all([user, user.comparePassword(passwordStr)])
+      }
+      done(null, null, {msg: `You're not in our system yet.  Try creating an account`})
+    })
+    .then((user, matches) => {
+      if (matches) {
+        done(null, user, {msg: `Welcome back!`})
+      } else {
+        done(null, null, {msg: `That password doesn't match our records...`})
+      }
+    })
+  .catch(done)
+  }
+);
+
+
+passport.use(localStrategy);
