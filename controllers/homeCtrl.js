8b651ee6f20efd34dfc5050cd54bd620f288d5a0
LucasMcL <LMcLaughlin09@gmail.com>
2017-04-12 13:03:18 -0500

    create method wip

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -1,13 +1,11 @@
 'use strict'
 
 const { User } = require('../models/userMd')
+const { Like } = require('../models/likesMd')
 
-const CURRENT_USER_ID = 2
-
-
+const CURRENT_USER_ID = 1
 
 module.exports.show = (req, res) => {
-
 	Promise.all([
 		User.getCurrentUser(CURRENT_USER_ID),
 		User.getUsers()
@@ -15,4 +13,9 @@ module.exports.show = (req, res) => {
 		const [currentUser, users] = values
 		res.render('home', {page: 'Home', users, currentUser})
 	})
+}
+
+module.exports.create = (req, res) => {
+	console.log('req.body:', req.body)
+	res.end()
 }
