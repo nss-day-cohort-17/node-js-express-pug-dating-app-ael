e43974f30afb0bc216b6ae78092d0fae84a2bb14
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-12 13:59:39 -0500

    swapped icons

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,8 +1,15 @@
 console.log('*********** CLICK SOMEBODY - START A RELATIONSHIP ***********');
 
-
 $('.icon').click((e) => {
+  // stores parent id
   let clickedID = e.target.parentNode.parentNode.parentNode.id;
-  console.log(clickedID);
-  $(`#${clickedID} .card-like-sect`).replaceWith(`<a class="icon"><img class="icon-heart" src="/img/add-to-favourites.png" alt="icon"/></a>`);
+
+  console.dir(e.target);
+  // evaluates target class name to swap innerHTML
+  if (e.target.className === 'icon-like') {
+    $(`#${clickedID} .card-like-sect`).replaceWith(`<div class="card-like-sect"><a class="icon"><img class="icon-heart" src="/img/heart-red.png" alt="icon"/></a></div>`);
+  } else {
+    $(`#${clickedID} .card-like-sect`).replaceWith(`<div class="card-like-sect"><a class="icon"><img class="icon-like" src="/img/heart-white.png" alt="icon"/></a></div>`);
+  }
+
 })
